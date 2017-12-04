9fad4122ea38f76bf8e5daa2197ab7f324a41b99
Robb Bowes <up666724@myport.ac.uk>
2017-12-04 12:21:21 +0000

    ingredient model added and implemented in list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Dummy name", "Dummy description", "https://c1.staticflickr.com/5/4149/4962298166_e8fca691a5_b.jpg")
+  ];
 
   constructor() { }
 
