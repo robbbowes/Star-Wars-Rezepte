357648d97b71bfc1fa7aacc4847288ec3ed18161
Robb Bowes <up666724@myport.ac.uk>
2017-12-12 13:53:13 +0000

    Can add ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe("Dummy name", "Dummy description", "https://c1.staticflickr.com/5/4149/4962298166_e8fca691a5_b.jpg")
+    new Recipe("Dummy name", "Dummy description", "https://c1.staticflickr.com/5/4149/4962298166_e8fca691a5_b.jpg"),
+    new Recipe("Dummy name 2", "Dummy description 2", "https://c1.staticflickr.com/5/4149/4962298166_e8fca691a5_b.jpg")    
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
